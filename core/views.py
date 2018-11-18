c53a11e8095e28cff6e167d9488735984e97e432
Adit Shah <aditshah00@gmail.com>
2018-11-18 07:14:42 -0800

    Lots of changes to django and html

--- core/views.py
+++ core/views.py
@@ -1,24 +1,41 @@
+from datetime import date, datetime
+import time
 from django.core.files.storage import FileSystemStorage
 from django.core.files.base import ContentFile
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.template import RequestContext
 from django.core.files.storage import default_storage
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
-
+from threading import Thread
 from .models import Document
 from .forms import DocumentForm
+import os
+
+filepath = ""
+
 
 def doStuff(file):
-    path = default_storage.save('tmp/' + str(file), ContentFile(file.read()))
-    print(path)
+    #text = predict(file)
+    print(os.getcwd())
+    time.sleep(2)
+    with open(file) as f:
+        response = HttpResponse(f.read())
+        return response
+
 
 def loading(request):
+    t = Thread(target=doStuff, args=(filepath,))
+    t.start()
     return list(request, url='loading.html')
+    time.sleep(1000)
+    #TODO: Add some ML code
+    #TODO: Make a done page + auto download
 
 def notjpg(request):
     return list(request, url='notjpg.html')
+
 
 def list(request, url='list.html'):
     # Handle file upload
@@ -27,6 +44,9 @@ def list(request, url='list.html'):
         if str(request.FILES['docfile']).split(".")[-1].lower() in ['jpg', 'jpeg']:
             newdoc = Document(docfile=request.FILES['docfile'])
             newdoc.save()
+            global filepath
+            d = datetime.now()
+            filepath = d.strftime("media/documents/%Y/%m/%d/") + str(request.FILES['docfile'])
             return HttpResponseRedirect(reverse('loading'))
         else:
             return HttpResponseRedirect(reverse('notjpg'))
